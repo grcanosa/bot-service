81093ddbe410f9b881ce68092fa61bb7356fee55
grcanosa <grcanosa@gmail.com>
2019-08-04 11:25:17 +0200

    Fixed different messages forward

--- services/bots/src/main/scala/com/grcanosa/telegrambot/bot/BotWithAdmin.scala
+++ services/bots/src/main/scala/com/grcanosa/telegrambot/bot/BotWithAdmin.scala
@@ -4,7 +4,7 @@ import akka.actor.{Actor, Props}
 import com.bot4s.telegram.api.{AkkaDefaults, Polling, TelegramBot}
 import com.bot4s.telegram.api.declarative.{Commands, Messages}
 import com.bot4s.telegram.clients.AkkaHttpClient
-import com.bot4s.telegram.methods.{SendMessage, SendPhoto}
+import com.bot4s.telegram.methods.{MultipartRequest, SendAnimation, SendAudio, SendMessage, SendPhoto, SendVideo, SendVideoNote, SendVoice}
 import com.bot4s.telegram.models.{InputFile, Message}
 import com.grcanosa.telegrambot.bot.BotWithAdmin.ForwardMessageTo
 import com.grcanosa.telegrambot.bot.user.UserHandler
@@ -117,12 +117,22 @@ with Commands{
     override def receive = {
       case sm: SendMessage => request(sm)
       case sp: SendPhoto => request(sp)
+      case sa: SendAnimation => request(sa)
+      case sa: SendAudio => request(sa)
+      case sv: SendVideo => request(sv)
+      case svn: SendVideoNote => request(svn)
+      case sv: SendVoice => request(sv)
       case ForwardMessageTo(cid,msg) => {
         msg.text.foreach(s => self ! SendMessage(cid,s))
         msg.photo.foreach(s => self ! SendPhoto(cid
                               ,InputFile(s.reverse.head.fileId)
                               ,caption=msg.caption)
                         )
+        msg.animation.foreach( s => self ! SendAnimation(cid,InputFile(s.fileId),replyToMessageId = None))
+        msg.audio.foreach(s => self ! SendAudio(cid,InputFile(s.fileId)))
+        msg.video.foreach(s => self ! SendVideo(cid,InputFile(s.fileId),duration=Some(s.duration)))
+        msg.videoNote.foreach(s => self ! SendVideoNote(cid,InputFile(s.fileId)))
+        msg.voice.foreach(s => self ! SendVoice(cid,InputFile(s.fileId)))
       }
       case _ =>
     }
